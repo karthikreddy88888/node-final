a4c6dc42be93f5b254769bd56cc6730e22a78eac
Karthikreddy88888 <webdevelopment40110668@gmail.com>
2025-08-13 20:41:37 +0530

    lastcommit1

--- Kambaz/Database/index.js
+++ Kambaz/Database/index.js
@@ -1,6 +1,6 @@
 import courses from "./courses.js";
 import modules from "./modules.js";
-import assignments from "./Assignments.js";
+import assignments from "./assignments.js";
 import enrollments from "./enrollments.js";
 import users from "./users.js"
 import quizzes from "./quizzes.js"
